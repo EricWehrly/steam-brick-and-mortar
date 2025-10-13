4881c4e0a80de1aa417236328941f9ff8dd2b118
EricWehrly <eric.wehrly@gmail.com>
2025-10-13 11:24:09 -0700

    remove unused

--- client/test/integration/startup.int.test.ts
+++ client/test/integration/startup.int.test.ts
@@ -337,7 +337,6 @@ describe('Application Startup Integration', () => {
             new SteamWorkflowManager(
                 mockEventManager,
                 mockSteamIntegration,
-                mockSceneCoordinator,
                 mockSteamUICoordinator
             )
         }).not.toThrow()
