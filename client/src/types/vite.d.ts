b4fe402da5fe75d167588d002a52a73bc017df81
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2025-11-08 10:21:01 -0800

    Add type declarations for vertex, fragment, and GLSL raw imports

--- client/src/types/vite.d.ts
+++ client/src/types/vite.d.ts
@@ -13,3 +13,18 @@ declare module '*.md?raw' {
     const content: string
     export default content
 }
+
+declare module '*.vert?raw' {
+    const content: string
+    export default content
+}
+
+declare module '*.frag?raw' {
+    const content: string
+    export default content
+}
+
+declare module '*.glsl?raw' {
+    const content: string
+    export default content
+}
